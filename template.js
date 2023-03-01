361e1bba9da11ab6f63ba8e9e61dcb816d43c4ae
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-03-01 16:55:59 +0600

    first commit

---
+++ template.js
@@ -0,0 +1,13 @@
+const person = 'Adam Sandler';
+const person2 = "Ben White";
+const person3 = `Donald Trump`;
+
+const multiline = 'first line \n'+'second line \n'
++'third line \n'+'fourth line';
+// console.log(multiline)
+
+const newMultiline = `first line
+second line
+third line
+fourth line`;
+console.log(newMultiline)
