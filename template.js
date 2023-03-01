77df3d1daa730a0668d9b14ce20a3fa8eab276fc
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-03-01 17:03:02 +0600

    first commit

--- template.js
+++ template.js
@@ -10,4 +10,12 @@ const newMultiline = `first line
 second line
 third line
 fourth line`;
-console.log(newMultiline)
+// console.log(newMultiline)
+
+const a = 20;
+const b = 30
+const summary = 'sum of: '+ a + ' and '+ b + ' is: '+(a+b);
+// console.log(summary);
+
+const result = `sum of ${a} and ${b} is: ${a+b}`;
+console.log(result)
